26aa48e7bb1e5673a9e1af19b55e37683a8e4f19
sunil-20 <sunil.kafle@hotmail.com>
2023-09-01 11:39:49 -0400

    Added lect 24.py

---
+++ Day2/-24_NumberManipulation.py
@@ -0,0 +1,30 @@
+
+# rounding the numbers. ,2(number of decimal places to round.)
+print(round(8/3, 2))
+print(round(2.66666666666666,2))
+
+# floor division, excludes values after decimal point
+
+print(8//3) # result 2
+
+result = 4/2
+
+result/=2
+print(result)
+
+score = 0
+score +=2
+score-=1
+score *= 5
+print(score)
+
+# f-strings
+
+print(f"Your score is: {score}")
+
+height = 1.8
+isWinning = True
+
+# use all data types in f-string.
+
+print(f"Your score is: {score}, Your height is: {height}, and your winning is: {isWinning}")
