ae8681e1d7e79eab36b4ac6fa5a89ef6e88be631
sunil-20 <sunil.kafle@hotmail.com>
2023-09-01 11:24:17 -0400

    BMI.py changed

--- Day2/BMI.py
+++ Day2/BMI.py
@@ -2,5 +2,5 @@
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 
-BMI = float(weight)/ float(float(height)**2)
+BMI = float(weight)/float(height)**2
 print(int(BMI))
