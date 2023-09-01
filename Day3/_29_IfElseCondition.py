bef55b4d833d63388fd5a7605f686dedc4aeb772
sunil-20 <sunil.kafle@hotmail.com>
2023-09-01 12:51:17 -0400

    push readme again

---
+++ Day3/_29_IfElseCondition.py
@@ -0,0 +1,18 @@
+# Control Flow with if /else and Conditional Operators
+
+# water_level = int(input("What is the expected water level? "))
+
+# if water_level > 80:
+#     print("Drain water.")
+# else:
+#     print("Continue filling the tub.")
+
+
+# Rollercoaster ride
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+    print("You can ride the rollercoaster.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
