183a3aa33ff8a8ad8c96279957775cea46e4d9b5
sunil-20 <sunil.kafle@hotmail.com>
2023-09-02 15:32:49 -0400

    Day4 Random Module lect 41

---
+++ Day4/_41_RandomModule.py
@@ -0,0 +1,11 @@
+# Randomisation in python
+import random
+# import the python file my_module to use as a python module for pi.
+import my_module
+# random_integer = random.randint(1,10)
+# print(random_integer)
+# print(my_module.pi)
+# print(my_module.name)
+
+random_float = random.random()
+print(random_float)
