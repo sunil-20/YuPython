6be49109650f24046ce331b286d4e60e8d714a61
sunil-20 <sunil.kafle@hotmail.com>
2023-09-02 15:42:10 -0400

    HeadTail added

---
+++ Day4/_42_HeadTail.py
@@ -0,0 +1,10 @@
+# Welcome to the coin toss..
+import random
+print("Welcome to the Coin TOSS! ")
+random_number = random.randint(1,2)
+
+if random_number == 1:
+    print("Head")
+else:
+    print("Tail")
+    
