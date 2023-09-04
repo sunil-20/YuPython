bb0e5c5b037813ec934bb53612f08389e2b69225
sunil-20 <sunil.kafle@hotmail.com>
2023-09-04 11:26:06 -0400

    51 changed

--- Day5/_51_AverageHeight.py
+++ Day5/_51_AverageHeight.py
@@ -4,12 +4,12 @@ student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 print(student_heights)
-total = 0
-length = 0
+total_height = 0
+number_of_students = 0
 for n in student_heights:
-    total += n
-    length+=1
+    total_height += n
+    number_of_students +=1
 # or you can use sum(student_heights) function to add the items of the list.
 #average_height = round(total / len(student_heights),2) # using len function.
-average_height = round(total/length)
+average_height = round(total_height/number_of_students)
 print(f"The average height is: {average_height}")
