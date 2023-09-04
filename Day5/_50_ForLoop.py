a3e36d30321de0a24c2df3d1eba3376211c416b5
sunil-20 <sunil.kafle@hotmail.com>
2023-09-04 10:00:14 -0400

    50 For Loop added

---
+++ Day5/_50_ForLoop.py
@@ -0,0 +1,10 @@
+#Day 5 python codes....
+""" for items in list_of_items:
+        #Do something to each item
+"""
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit) # prints the item inside the list.
+    print(fruit + " Pie") # print the item and additional string each time.
+    print(fruits) # inside the for loop, print fruits list until loop finished printing the values inside.
+print(fruits) # outside the for loop. It prints the fruits list.
