5aa3270011f5f082395e92aea536141ba23397f0
SK <sunil.kafle@hotmail.com>
2023-09-05 07:02:30 -0400

    54 added

---
+++ Day5/_54_AddEven.py
@@ -0,0 +1,14 @@
+# Adding Even Numbers 1 to 100 including 1
+
+total = 0
+for number in range(1,101):
+    if number == 1 or number % 2 ==0:
+        total+= number
+print(total)
+
+# way 2 without adding even 1 to 100
+total2 = 0
+for number2 in range (2, 101, 2):
+    total2 += number2
+print(total2)
+
