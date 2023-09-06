c40e55fdbc21d84de95564bcca3fcfce7827aecf
SK <sunil.kafle@hotmail.com>
2023-09-06 11:40:30 -0400

    added for loop in the 69

--- Day7/_69_RandWordWithAns.py
+++ Day7/_69_RandWordWithAns.py
@@ -3,8 +3,10 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 guess = input("What letter do you guess?\n").lower()
-
-if guess in chosen_word:
-    print("Yes")
-else:
-    print("No")
+# to check every occurance you have to use for loop
+# if else will check just the first occurance.
+for letter in chosen_word:
+    if letter == guess:
+        print("Yes")
+    else:
+        print("No")
