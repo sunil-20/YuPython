61e5b838f01bd60bfe3ee909daf66ba62c142092
SK <sunil.kafle@hotmail.com>
2023-09-06 14:50:40 -0400

    WinCheck changed

--- Day7/_73_WinCheck.py
+++ Day7/_73_WinCheck.py
@@ -1,13 +1,13 @@
 # Checking if the player have won!
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
+# create blank list
 display = []
 for l in range(len(chosen_word)):
     display += "_"
-print(display)
+#condition to start and end the while loop
 end_of_game = False
 while not end_of_game:
     guess = input("What letter do you guess?\n").lower()
@@ -16,7 +16,7 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
     print(display)
-
+# condition for ending the while loop after the guessing is completed.
     if "_" not in display:
         end_of_game = True
         print("You win")
