1a5d78aa32897d4d0e1850b99b9f61702893aceb
SK <sunil.kafle@hotmail.com>
2023-09-07 12:34:35 -0400

    88 added

---
+++ Day8/_88_CCPart4.py
@@ -0,0 +1,33 @@
+# Caesar Cipher Encryption 4
+# fixing bugs
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# repeat the program
+should_end = False
+while not should_end:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def caesar(text, shift, direction):
+        end_text = ""
+        for i in text:
+            letter_location = alphabet.index(i)
+            if direction == "encode":
+                end_text += alphabet[letter_location + shift]
+            else:
+                end_text += alphabet[letter_location - shift]
+        if direction == "encode":
+            print(f"Here is the encoded message: {end_text}")
+        else:
+            print(f"Here is the decoded message: {end_text}")
+
+    caesar(text= text, shift=shift, direction = direction)
+    restart = input("Do you want to  start new encryption? Yes or No").lower()
+    if restart == 'no':
+        should_end = True
+
