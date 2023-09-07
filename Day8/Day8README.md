89bd33cd3b6f9f58738e3107fbe755e99f55ce18
SK <sunil.kafle@hotmail.com>
2023-09-06 22:17:34 -0400

    New files added

--- Day8/Day8README.md
+++ Day8/Day8README.md
@@ -1,1 +1,4 @@
 ### Beginner Function Parameters & Caesar Cipher
+* something = 123 ( Something is parameter, 123 is argument)
+* argument is the actual piece of data that is passed over to the function.
+* parameter is the name of the data.
--- Day8/_81_FxwthInput.py
+++ Day8/_81_FxwthInput.py
@@ -1,2 +1,10 @@
 def greet():
     print("Hello")
+    print("Isn't the weather nice today?")
+greet()
+
+# Function that allows for the input
+def greet_with_name(name):
+    print(f"Hello {name}!")
+    print(f"How do you do {name}!")
+greet_with_name("Taylor")
