33179f8390efa771ff09906e58349d0a0ba193b7
SK <sunil.kafle@hotmail.com>
2023-09-07 10:57:27 -0400

    PCadde

--- Day8/_84_PrimeChecker.py
+++ Day8/_84_PrimeChecker.py
@@ -1,13 +1,11 @@
 # function checking a prime number.
-
-
 def prime_checker(number):
     is_prime = True
     for i in range(2, number):
         if number_to_check % i == 0:
             is_prime = False
     if is_prime == False:
-        print(f" The number {number_to_check} is not a prime number")
+        print(f" The number {number_to_check} is not a prime number.")
     else:
         print(f"The number {number_to_check} is a prime number.")
 
