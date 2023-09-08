0817810d9c826bbd27241e477d6749663a14d295
SK <sunil.kafle@hotmail.com>
2023-09-08 14:06:38 -0400

    Code changed

--- Day10/_105_WhileRecursionFlags.py
+++ Day10/_105_WhileRecursionFlags.py
@@ -1,3 +1,5 @@
+from art import logo
+
 # define add
 def add(n1, n2):
     return n1+n2
@@ -21,28 +23,23 @@ operations = {
     "/": divide
 }
 def calculator():
-    num1 = int(input("What's the first number?: "))
+    #print(logo)
+    num1 = float(input("What's the first number?: "))
     for symbol in operations:
         print(symbol)
     operation_symbol = input("Pick an operation from the list above: ")  
-    num2 = int(input("What is the next number?: "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-    add_numbers = input("Do you want to add more numbers? yes or no/n").lower()
-    while add_numbers == "yes":
-    # pick another operation so you can use the previous operation as input using return
-        operation_symbol = input("Pick an operation from the list above: ")  
-        answer_in = answer
-        num3 = int(input("What is the number?: "))
+    should_continue = True
+    while should_continue:
+        num2 = float(input("What is the next number?: "))
         calculation_function = operations[operation_symbol]
-        answer_final = calculation_function(answer_in, num3)
-        print(f"{answer_in} {operation_symbol} {num3} = {answer_final}")
-        add_numbers = input("Do you want to add more numbers? yes or no/n").lower()
-        answer_in = answer_final
-
-    restart = input("Do you want another calculation to perform? yes or no/n")
-    if restart == "yes":
-        calculator()
-        print("Thank you for using calculator app!")
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        new_action = input(f"Type 'y' to continue with {answer}, or type 's' to stop or 'r' to restart: ").lower()
+        if new_action == 'y':
+            num1 = answer
+        elif new_action == 's':
+            should_continue = False
+        else:
+            print("Thank you for using calculator app!")
+            calculator()
+calculator()
