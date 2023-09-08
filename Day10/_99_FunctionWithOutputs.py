952a797a19ed9f2ad01609cdd2a34bdc4a7ea354
SK <sunil.kafle@hotmail.com>
2023-09-08 10:39:56 -0400

    99 changed

--- Day10/_99_FunctionWithOutputs.py
+++ Day10/_99_FunctionWithOutputs.py
@@ -1,9 +1,26 @@
 # Functions with Outputs Lect 99
 """
-def my_function():
-    #Do this
+def my_function(something): # something is parameter
+    #Do this with something
     #Then do this
     #Finally do this
+
+#call the fx
+my_function(123) # 123 is argument
     
 """
 
+# fx with outputs
+f_name = input("What is your name?\n")
+l_name = input("What is your last name?\n")
+def format_name(f_name, l_name):
+    final_name = f_name.title() +" "+ l_name.title()
+    print(final_name)
+format_name(f_name= f_name, l_name= l_name)
+# code changes to return
+f_name = input("What is your name?\n")
+l_name = input("What is your last name?\n")
+def format_name(f_name, l_name):
+    return f_name.title() +" "+ l_name.title()
+
+print(format_name(f_name= f_name, l_name= l_name))
