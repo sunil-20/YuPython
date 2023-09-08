d8459b664f40fa72bb33e4249056fcb42defa784
SK <sunil.kafle@hotmail.com>
2023-09-08 14:29:46 -0400

    105 changed

--- Day10/_105_WhileRecursionFlags.py
+++ Day10/_105_WhileRecursionFlags.py
@@ -27,19 +27,20 @@ def calculator():
     num1 = float(input("What's the first number?: "))
     for symbol in operations:
         print(symbol)
-    operation_symbol = input("Pick an operation from the list above: ")  
     should_continue = True
+    
     while should_continue:
+        operation_symbol = input("Pick an operation from the list above: ")  
         num2 = float(input("What is the next number?: "))
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
         new_action = input(f"Type 'y' to continue with {answer}, or type 's' to stop or 'r' to restart: ").lower()
         if new_action == 'y':
-            num1 = answer
+            num1 = answer 
         elif new_action == 's':
+            print("Thank you for using calculator app!")
             should_continue = False
         else:
-            print("Thank you for using calculator app!")
             calculator()
 calculator()
