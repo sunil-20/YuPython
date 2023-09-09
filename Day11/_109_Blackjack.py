3136fb14e6d5bca03b0733dae3d4bb5275af85b3
SK <sunil.kafle@hotmail.com>
2023-09-08 23:42:13 -0400

    Changed

--- Day11/_109_Blackjack.py
+++ Day11/_109_Blackjack.py
@@ -1,7 +1,7 @@
 # Blackjack capstone
 import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-#dealers_draw = random.choice(cards)
+# dealing with dealers cards.
 dealers_cards = []
 dealers_total = 0
 dealers_draw = True
@@ -12,6 +12,19 @@ while dealers_draw:
         dealers_total += new_card
     else:
         dealers_draw = False
-print(dealers_cards)
-print(dealers_total)
-#computer_card = random.choice(cards)
+print(f"Dealers cards: {dealers_cards}")
+print(f" Dealers total: {dealers_total}")
+
+# dealing with opponents card.
+opponents_cards = []
+opponents_total = 0
+opponents_draw = True
+while opponents_draw:
+    if opponents_total < 17:
+        new_card_opp = random.choice(cards)
+        opponents_cards.append(new_card_opp)
+        opponents_total += new_card_opp
+    else:
+        opponents_draw = False
+print( f"Opponents cards: {opponents_cards}")
+print(f"Opponents total: {opponents_total}")
