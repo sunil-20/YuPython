27897db1435dd8fbccf1c1b96a55567713800676
SK <sunil.kafle@hotmail.com>
2023-09-09 22:44:39 -0400

    code changed

--- Day11/blackjack_final.py
+++ Day11/blackjack_final.py
@@ -5,3 +5,15 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
+# function to return 
+def calculate_score(cards):
+    """ Return the total from the card on hand."""
+    # check for blackjack == 21
+    if sum(cards)==21 and len(cards)>2:
+        return 0
+    if 11 in cards and sum(cards) >21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+# comparing user and computer
