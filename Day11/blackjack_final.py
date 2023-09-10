9855a3260093c5cdf4264c0b37d2c8b2dc945cfb
SK <sunil.kafle@hotmail.com>
2023-09-09 22:39:17 -0400

    Final file added

---
+++ Day11/blackjack_final.py
@@ -0,0 +1,7 @@
+import random
+from art import logo
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
