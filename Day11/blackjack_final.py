53989c228175aab901be9fe6bdf22e833f19aec3
SK <sunil.kafle@hotmail.com>
2023-09-09 23:24:38 -0400

    Code changed

--- Day11/blackjack_final.py
+++ Day11/blackjack_final.py
@@ -9,18 +9,19 @@ def deal_card():
 def calculate_score(cards):
     """ Return the total from the card on hand."""
     # check for blackjack == 21
-    if sum(cards)==21 and len(cards)>2:
+    if sum(cards) == 21 and len(cards) == 2:
         return 0
-    if 11 in cards and sum(cards) >21:
+    if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
     return sum(cards)
 
 # comparing user and computer
 def compare(user_score, computer_score):
+    # busted with >21
     if user_score > 21 and computer_score > 21:
         return " Game over >21"
-    
+    # other conditions
     if user_score == computer_score:
         return "Draw"
     elif computer_score ==0:
@@ -52,13 +53,13 @@ def play_game():
         print(f" Computer's first card: {computer_cards[0]}")
 
         if user_score == 0 or computer_score ==0 or user_score >21:
-            is_game_over = True
+            game_over = True
         else:
             next_deal = input("Type 'y' to get another card, type 'n' to pass: ")
             if next_deal == "y":
                 user_cards.append(deal_card())
             else:
-                is_game_over = True
+                game_over = True
     while computer_score != 0 and computer_score <17:
         computer_cards.append(deal_card())
         computer_score = calculate_score(computer_cards)
