ced3643054b6d4d9c3d7289b9df962b152b102f2
SK <sunil.kafle@hotmail.com>
2023-09-09 23:31:22 -0400

    Code changed

--- Day11/blackjack_final.py
+++ Day11/blackjack_final.py
@@ -55,7 +55,7 @@ def play_game():
         if user_score == 0 or computer_score ==0 or user_score >21:
             game_over = True
         else:
-            next_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            next_deal = input(" Type 'y' to get another card, type 'n' to pass: ")
             if next_deal == "y":
                 user_cards.append(deal_card())
             else:
