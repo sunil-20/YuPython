59429145f6b2bcc08af861a342bd8465b2be7275
SK <sunil.kafle@hotmail.com>
2023-09-11 00:08:00 -0400

    code changed

--- Day12/_120_NumberGuessing.py
+++ Day12/_120_NumberGuessing.py
@@ -2,8 +2,6 @@
 import random
 from art import logo
 print(logo)
-game_stop = False
-#while not game_stop:
 def guess_me():
         print("Welcome to the number guessing game")
         print("I am thinking a number between 1 and 50")
@@ -20,13 +18,16 @@ def guess_me():
             guess = int(input("What number do you guess?: "))
             total_try-=1
             if guess > number:
-                print(f"You guessed too high. \n You have {total_try} attempts remaining.")
+                print(f"You guessed too high. \nYou have {total_try} attempts remaining.")
             elif guess == number:
                 print(f"You guessed corret!")
                 game_stop = True
             else:
-                print(f"You guessed too low. \n You have {total_try} remaining.")
-            if total_try ==0:
-                print("You run out of guesses, you lose!")
+                print(f"You guessed too low. \nYou have {total_try} remaining.")
+            if total_try ==0 and guess != number:
+                print("You run out of guesses! You lose")
                 game_stop = True
+            elif total_try == 0 and guess == number:
+                 print("You ran out of the guesses but you win at the very end!")
+                 game_stop = True
 guess_me()
