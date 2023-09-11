88ddfcdb1ca072c32af56a2bcb0f0ac8d4b4e83c
SK <sunil.kafle@hotmail.com>
2023-09-10 21:41:12 -0400

    Code adde

---
+++ Day12/_116_LocalGlobal.py
@@ -0,0 +1,18 @@
+
+apple_picking = 3
+
+def apple_picking():
+  apple_picking = 5
+  print(f"Apples picking inside the boundary: {apple_picking}")
+
+apple_picking()
+print(f"Apples picking outside the boundary: {apple_picking}")
+
+enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
