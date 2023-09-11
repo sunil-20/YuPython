06fcc218ff7fce68da9c19e174ab901035e07774
SK <sunil.kafle@hotmail.com>
2023-09-11 11:00:18 -0400

    files added

---
+++ Day13/_131_DebugLeapYear.py
@@ -0,0 +1,15 @@
+# Debugging leap year
+# year hasn't casted as int. str and int can't compared.
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
---
+++ Day13/_132_DebugFizzBuzz.py
@@ -0,0 +1,23 @@
+#FizzBuzz Debug original code
+"""
+for number in range(1, 101):
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  if number % 3 == 0:
+    print("Fizz")
+  if number % 5 == 0:
+    print("Buzz")
+  else:
+    print([number])
+    """
+
+# solution
+for number in range(1, 101):
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  if number % 3 == 0:
+    print("Fizz")
+  if number % 5 == 0:
+    print("Buzz")
+  else:
+    print([number])
