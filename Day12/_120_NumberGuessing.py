3f2019d622cb779146ce59595d4233d5d368804a
SK <sunil.kafle@hotmail.com>
2023-09-10 23:11:16 -0400

    120 changed

--- Day12/_120_NumberGuessing.py
+++ Day12/_120_NumberGuessing.py
@@ -1,3 +1,16 @@
 # Number Guessing game
+import random
 from art import logo
 print(logo)
+total_try = ""
+if 
+
+
+def guess_me():
+    print("Welcome to the number guessing game")
+    print("I am thinking a number between 1 and 50")
+    number = random.randint(1, 50) # a and b inclusive
+    print(f"The guessed number is: {number}.")
+    guess = int(input("What number do you guess?: "))
+
+guess_me()
