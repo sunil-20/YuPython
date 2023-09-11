88c932c7e19dddd65948d1b88b71f6250f9d7e20
SK <sunil.kafle@hotmail.com>
2023-09-11 10:33:44 -0400

    130 added

---
+++ Day13/_130_OddEvenDebug.py
@@ -0,0 +1,8 @@
+
+number = int(input("Which number do you want to check?"))
+# assignment operator(=) used instead of comparision(==) operator.
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
