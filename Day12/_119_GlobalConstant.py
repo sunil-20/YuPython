b9f9065295a479a546f851d6224014995beb9dba
SK <sunil.kafle@hotmail.com>
2023-09-10 22:37:26 -0400

    Code changed

--- Day12/_119_GlobalConstant.py
+++ Day12/_119_GlobalConstant.py
@@ -1,8 +1,9 @@
-# the you don't want to change
+# you don't want to change the values of constants.
 # to differenciate the constants, the naming convention is UPPERCASE.
 PI = 3.1416
 URL = "https://www.google.com"
 TWITTER_HANDLE = "@skvizon"
+SHAPE = "Circle"
 
 def calc():
     TWITTER_HANDLE
