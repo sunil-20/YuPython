42b84b0cc6fd2eb6af108e1078f1a70a0f9300d9
SK <sunil.kafle@hotmail.com>
2023-09-10 22:36:02 -0400

    119 added

---
+++ Day12/_119_GlobalConstant.py
@@ -0,0 +1,8 @@
+# the you don't want to change
+# to differenciate the constants, the naming convention is UPPERCASE.
+PI = 3.1416
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@skvizon"
+
+def calc():
+    TWITTER_HANDLE
