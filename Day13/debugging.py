d2cd13beb796b70e882a7e3fc5ba1a871a21f4c4
SK <sunil.kafle@hotmail.com>
2023-09-11 10:24:44 -0400

    debugging completed

--- Day13/debugging.py
+++ Day13/debugging.py
@@ -8,11 +8,11 @@
 # my_function()
 
 # solution range changed to 21
-# def my_function():
-#   for i in range(1, 21):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
 
 # # Reproduce the Bug
 # from random import randint
@@ -24,10 +24,10 @@
 #Solution, The index value 6 is out of range
 # randint is inclusive(1,6)
 # change the number 6 to 5 as a solution
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(0, 5)
-# print(dice_imgs[dice_num])
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
@@ -38,11 +38,11 @@
 
 #solution, play computer
 #problem: 1994 has not been catched by if elif block.
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year <= 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+year = int(input("What's your year of birth?"))
+if year > 1980 and year <= 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
 
 # Fix the Errors
 # age = input("How old are you?")
@@ -51,12 +51,11 @@
 
 # Solution, Fix the Errors
 #problem: indentation error, type error, f string missing, else statement missing to capture
-# age <18.
 age = int(input("How old are you?"))
-# if age >= 18:
-#     print(f"You can drive at age {age}.")
-# else:
-#     print(f"You can't drive at age {age}.")
+if age >= 18:
+    print(f"You can drive at age {age}.")
+else:
+    print(f"You can't drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
@@ -67,10 +66,11 @@ age = int(input("How old are you?"))
 # print(total_words)
 
 # #Solution, Print is Your Friend
+#problem double = signs
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
@@ -83,3 +83,14 @@ print(total_words)
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
+
+# #Solution, Use a Debugger
+#problem: append outside the for loop
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
