ce625e796c084dd06f1b9cfbd4df8372c7f056ba
SK <sunil.kafle@hotmail.com>
2023-09-10 21:56:58 -0400

    Code changed

--- Day12/_116_LocalGlobal.py
+++ Day12/_116_LocalGlobal.py
@@ -7,3 +7,13 @@ def apple_picking():
 
 apple_picking()
 print(f"Apples picking outside the boundary: {apple_picked}")
+
+#Local scope kitchen
+def  make_sandwich():
+  # local variable in scope of kitchen
+  bread = "White pan bread"
+  cheese = "Colby Jack cheese"
+  mayonnaise = "Hellmans mayonnaise"
+  print(f"Making a sandwich with {bread}, {cheese}, and {mayonnaise}.")
+make_sandwich()
+print(bread) # this will not be printed. Error
