219432dadd3c4d5a4f96e6a150afcdfb038c89f3
SK <sunil.kafle@hotmail.com>
2023-09-11 11:24:21 -0400

    changed code

--- Day13/_130_DebugOddEven.py
+++ Day13/_130_DebugOddEven.py
@@ -1,23 +1,9 @@
 
-# number = int(input("Which number do you want to check?"))
-# # assignment operator(=) used instead of comparision(==) operator.
-# if number % 2 == 0:
-#   print("This is an even number.")
-# else:
-#   print("This is an odd number.")
-  
-# Debugging leap year
-
-year = int(input("Which year do you want to check?"))
-
-if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print("Leap year.")
-    else:
-      print("Not leap year.")
-  else:
-    print("Leap year.")
+number = int(input("Which number do you want to check?"))
+# assignment operator(=) used instead of comparision(==) operator.
+if number % 2 == 0:
+  print("This is an even number.")
 else:
-  print("Not leap year.")
+  print("This is an odd number.")
   
+
