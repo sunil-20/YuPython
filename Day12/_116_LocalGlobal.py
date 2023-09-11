75d91279d242793614bc0fefa79615aa3bc9bf64
SK <sunil.kafle@hotmail.com>
2023-09-10 21:43:23 -0400

    Code changed

--- Day12/_116_LocalGlobal.py
+++ Day12/_116_LocalGlobal.py
@@ -1,18 +1,9 @@
 
-apple_picking = 3
+apple_picked = 3
 
 def apple_picking():
-  apple_picking = 5
-  print(f"Apples picking inside the boundary: {apple_picking}")
+  apple_picked = 5
+  print(f"Apples picking inside the boundary: {apple_picked}")
 
 apple_picking()
-print(f"Apples picking outside the boundary: {apple_picking}")
-
-enemies = 1
-
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
-
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+print(f"Apples picking outside the boundary: {apple_picked}")
