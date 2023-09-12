369570ee68d7ea683e889a4082516f486c893b18
SK <sunil.kafle@hotmail.com>
2023-09-12 14:26:47 -0400

    Code changed

--- Day15/_142_CoffeeMachine.py
+++ Day15/_142_CoffeeMachine.py
@@ -6,21 +6,27 @@ if report_ingredients == "yes":
     print(f"Water: {resources['water']},\nMilk: {resources['milk']},\nCoffee: {resources['coffee']}")
 
 coffee_type = input("What would you like?(espresso/latte/cappuccino): ").lower()
-
-
-def coin_insert(q, d, n, p):
-    total_dollar = q * 0.25 + d * 0.10 + n * 0.05 + p * 0.01
-    return total_dollar
-
-
-print("Please insert coins.")
-quarters = int(input("How many quarters?: "))
-dimes = int(input("How many dimes?: "))
-nickles = int(input("How many nickles?: "))
-pennies = int(input("How many pennies?: "))
-
-total = coin_insert(q=quarters, d=dimes, n=nickles, p=pennies)
-
-
-print(total)
-print(coffee_type)
+if coffee_type == "espresso":
+    espresso_price = MENU['espresso']['cost']
+elif coffee_type == "latte":
+    latte_price = MENU['latte']['cost']
+else:
+    cappuccino_price = ['cappuccino']['cost']
+    print(espresso_price)
+#
+# def coin_insert(q, d, n, p):
+#     total_dollar = q * 0.25 + d * 0.10 + n * 0.05 + p * 0.01
+#     return total_dollar
+#
+#
+# print("Please insert coins.")
+# quarters = int(input("How many quarters?: "))
+# dimes = int(input("How many dimes?: "))
+# nickles = int(input("How many nickles?: "))
+# pennies = int(input("How many pennies?: "))
+#
+# total = coin_insert(q=quarters, d=dimes, n=nickles, p=pennies)
+#
+#
+# print(total)
+# print(coffee_type)
