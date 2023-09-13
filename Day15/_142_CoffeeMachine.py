802338d8fcbcf35294a87ba4fc62c46ec62655ef
SK <sunil.kafle@hotmail.com>
2023-09-12 23:11:41 -0400

    code changed

--- Day15/_142_CoffeeMachine.py
+++ Day15/_142_CoffeeMachine.py
@@ -6,32 +6,36 @@ if report_ingredients == "yes":
     print(f"Water: {resources['water']},\nMilk: {resources['milk']},\nCoffee: {resources['coffee']}")
 
 coffee_type = input("What would you like?(espresso/latte/cappuccino): ").lower()
-price = 0
+coffee_price = 0
 if coffee_type == "espresso":
     espresso_price = MENU['espresso']['cost']
-    price += espresso_price
+    coffee_price += espresso_price
 elif coffee_type == "latte":
     latte_price = MENU['latte']['cost']
-    price += latte_price
+    coffee_price += latte_price
 elif coffee_type == "cappuccino":
     cappuccino_price = MENU['cappuccino']['cost']
-    price += cappuccino_price
-print(f"Price of {coffee_type.title()} is: {price}")
+    coffee_price += cappuccino_price
+print(f"Price of {coffee_type.title()} is: {coffee_price}")
 
 
-# def coin_insert(q, d, n, p):
-#     total_dollar = q * 0.25 + d * 0.10 + n * 0.05 + p * 0.01
-#     return total_dollar
-#
-#
-# print("Please insert coins.")
-# quarters = int(input("How many quarters?: "))
-# dimes = int(input("How many dimes?: "))
-# nickles = int(input("How many nickles?: "))
-# pennies = int(input("How many pennies?: "))
-#
-# total = coin_insert(q=quarters, d=dimes, n=nickles, p=pennies)
-#
-#
-# print(total)
-# print(coffee_type)
+def coin_insert(q, d, n, p):
+    total_dollar = q * 0.25 + d * 0.10 + n * 0.05 + p * 0.01
+    return total_dollar
+
+
+print("Please insert coins.")
+quarters = int(input("How many quarters?: "))
+dimes = int(input("How many dimes?: "))
+nickles = int(input("How many nickles?: "))
+pennies = int(input("How many pennies?: "))
+
+total_fund = coin_insert(q=quarters, d=dimes, n=nickles, p=pennies)
+change = total_fund - coffee_price
+if total_fund < coffee_price:
+    print("You have insufficient fund to brew the coffee!")
+else:
+    print(f"Here is your change {change} ")
+    print("Enjoy the coffee")
+print(total_fund)
+print(coffee_type)
