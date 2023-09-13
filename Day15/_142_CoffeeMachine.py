c1bd42eefeaac66ac8ab56a2b8891e53ad94a605
SK <sunil.kafle@hotmail.com>
2023-09-12 22:53:56 -0400

    Code changed

--- Day15/_142_CoffeeMachine.py
+++ Day15/_142_CoffeeMachine.py
@@ -6,14 +6,19 @@ if report_ingredients == "yes":
     print(f"Water: {resources['water']},\nMilk: {resources['milk']},\nCoffee: {resources['coffee']}")
 
 coffee_type = input("What would you like?(espresso/latte/cappuccino): ").lower()
+price = 0
 if coffee_type == "espresso":
     espresso_price = MENU['espresso']['cost']
+    price += espresso_price
 elif coffee_type == "latte":
     latte_price = MENU['latte']['cost']
-else:
-    cappuccino_price = ['cappuccino']['cost']
-    print(espresso_price)
-#
+    price += latte_price
+elif coffee_type == "cappuccino":
+    cappuccino_price = MENU['cappuccino']['cost']
+    price += cappuccino_price
+print(f"Price of {coffee_type.title()} is: {price}")
+
+
 # def coin_insert(q, d, n, p):
 #     total_dollar = q * 0.25 + d * 0.10 + n * 0.05 + p * 0.01
 #     return total_dollar
