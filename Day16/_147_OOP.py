a59f7d0306980d4439b1851f4d36a3ff8bfe9af4
SK <sunil.kafle@hotmail.com>
2023-09-18 15:05:37 -0400

    code changed

--- Day16/_147_OOP.py
+++ Day16/_147_OOP.py
@@ -1,6 +1,9 @@
 import turtle
 timmy = turtle.Turtle()
 timmy.shape("turtle")
+timmy.color('blue')
+timmy.forward(100)
 my_screen = turtle.Screen()
+
 print(my_screen.canvheight)
 my_screen.exitonclick()
