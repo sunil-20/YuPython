783c151408cd19a58c760b42d1c8ad3b85b152c6
SK <sunil.kafle@hotmail.com>
2023-09-20 12:07:37 -0400

    code changed

--- Day16/Class_Corey.py
+++ Day16/Class_Corey.py
@@ -3,11 +3,23 @@
 
 # Create blueprient name = Employeefor creating instances.
 class Employee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@company.com"
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
 
 
-emp_1 = Employee()  # unique instance of the Employee class
-emp_2 = Employee()  # unique instance of the Employee class
+emp_1 = Employee("Jack", "Ma", 5000)  # unique instance of the Employee class
+emp_2 = Employee("Cody", "Lee", 1000)  # unique instance of the Employee class
 
-print(emp_1)
-print(emp_2)
+# print(emp_1)  # different location in the memory
+# print(emp_2)
+
+print(emp_1.email)
+
+# print full name without creating method inside a class
+print("{} {}".format(emp_1.first, emp_1.last))
+print(f"Name printed using method inside the class:\n{emp_2.fullname()}")
