c70ff3e4c00e3bd217444d168723dd14f15ac07c
SK <sunil.kafle@hotmail.com>
2023-09-20 11:41:08 -0400

    code changed

--- Day16/Class_Corey.py
+++ Day16/Class_Corey.py
@@ -1,8 +1,11 @@
 # Python Object-Oriented Programming
 # Employee = has attributes and methods (eg. name, email, position_title, pay)
 
-# Create blueprient for creating instances.
+# Create blueprient name = Employeefor creating instances.
 class Employee:
+    pass
     emp_1 = Employee()
     emp_2 = Employee()
 
+print(emp_1)
+print(emp_2)
