8e9ee147a2f6256af4b6f15211dea9bc35ae4cca
SK <sunil.kafle@hotmail.com>
2023-09-25 21:34:45 -0400

    code changed

--- Day16/_150_OOP_CoffeeMachine.py
+++ Day16/_150_OOP_CoffeeMachine.py
@@ -3,4 +3,9 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-print("Welcome to coffee maker!")
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+
+
