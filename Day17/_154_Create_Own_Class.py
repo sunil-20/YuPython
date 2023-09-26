1a83bb752054819192813804e180a1aa986ffe5a
SK <sunil.kafle@hotmail.com>
2023-09-25 22:34:53 -0400

    code changed

--- Day17/_154_Create_Own_Class.py
+++ Day17/_154_Create_Own_Class.py
@@ -3,6 +3,12 @@ class User:
     def __init__(self, user_id, username):
         self.user_id = user_id
         self.username = username
+        self.followers = 0  # default value, no need to provide parameter in init.
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        user.following += 1
 
 
 user_1 = User("001", "zombie")  # object
@@ -13,3 +19,12 @@ print(user_1.username)
 
 print(user_2.user_id)
 print(user_2.username)
+
+# call
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+
+print(user_2.followers)
+print(user_2.following)
