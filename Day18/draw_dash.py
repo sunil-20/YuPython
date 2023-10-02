61383b5ba3fac6447ed97a3a899ed514c2cb402a
SK <sunil.kafle@hotmail.com>
2023-10-02 13:41:22 -0400

    code changed

--- Day18/draw_dash.py
+++ Day18/draw_dash.py
@@ -6,7 +6,7 @@ timmy.shape("turtle")
 timmy.color("red")
 timmy.penup()
 timmy.setx(-300)
-timmy.sety(10)
+timmy.sety(50)
 timmy.pendown()
 #timmy.goto(-100, 1)
 
@@ -15,6 +15,15 @@ for i in range(50):
     timmy.forward(5)
     timmy.pencolor("white")
     timmy.forward(5)
+
+tom = Turtle()
+tom.shape('turtle')
+for _ in range(15):
+    tom.pendown()
+    tom.pencolor("red")
+    tom.forward(10)
+    tom.penup()
+    tom.forward(10)
 
 screen = Screen()
 screen.exitonclick()
