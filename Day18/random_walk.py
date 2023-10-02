785cd903031472e1af1d310b69ea28aa715cf25e
SK <sunil.kafle@hotmail.com>
2023-10-02 14:50:48 -0400

    new file added

---
+++ Day18/random_walk.py
@@ -0,0 +1,25 @@
+# create a random walk using turtle
+from turtle import Turtle, Screen
+import random
+
+timmy = Turtle()
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+direction = [0, 90, 180, 270]
+for _ in range(300):
+    timmy.speed(100)
+    timmy.width(4)
+    timmy.color(random.choice(colors))
+    timmy.forward(30)
+    timmy.setheading(random.choice(direction))
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
