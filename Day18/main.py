bc8042613703163694ef6123a0365f27b7191850
SK <sunil.kafle@hotmail.com>
2023-10-02 12:50:05 -0400

    code changed

--- Day18/main.py
+++ Day18/main.py
@@ -1,26 +1,33 @@
 # Turtle module
 from turtle import Turtle, Screen
-timmy_the_turtle = Turtle()
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
 
 
+def corner():
+    timmy.forward(100)
+    timmy.right(90)
+    # screen = Screen()
+    # screen.exitonclick()
 
 
+def square():
+    corner()
+    corner()
+    corner()
+    corner()
+    screen = Screen()
+    screen.exitonclick()
 
 
+# square()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# using for loop
+for i in range(4):
+    timmy.forward(100)
+    timmy.right(90)
 
 screen = Screen()
 screen.exitonclick()
