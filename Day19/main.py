327838034c9bebe94ff563d93c450634040d33dd
SK <sunil.kafle@hotmail.com>
2023-10-06 15:27:28 -0400

    code changed

--- Day19/main.py
+++ Day19/main.py
@@ -1,10 +1,30 @@
 from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
-
+# W = Forward,
+# S = Backwards, 
+# A = Counter_Clock, 
+# D = Clockwise
+# C = Clear drawing
 # create a function for movement
 def move_forward():
-    tim.forward(10)
+    tim.forward(50)
+def move_backward():
+    tim.backward(50)
+def counter_clock():
+    tim.left(50)
+
+def clear_screen():
+    tim.penup()
+    tim.clear()
+    tim.home()
+    
 screen.listen()
-screen.onkey(key = "space", fun= move_forward)
+screen.onkey(key = "w", fun = move_forward)
+screen.onkey(key = "s", fun = move_backward)
+screen.onkey(key = "a", fun = counter_clock)
+screen.onkey(key = "c", fun = clear_screen)
+
+
+
 screen.exitonclick()
