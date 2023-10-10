4ed83b8014f8a8487a1fd6b314ee0c548e613e87
SK <sunil.kafle@hotmail.com>
2023-10-10 15:11:08 -0400

    code changed

--- Day19/_179_ObjectStateInstance.py
+++ Day19/_179_ObjectStateInstance.py
@@ -3,15 +3,19 @@ from turtle import Turtle, Screen
 import random
 screen = Screen()
 tim = Turtle()
+tim.width = 4
 jim = Turtle()
 kelly = Turtle()
 miya = Turtle()
 pix = 500
+tim.color = "green"
+jim.color = "purple"
+kelly.color = "blue"
 
 def move_forward():
     screen.listen()
     screen.onkey(key = "w", fun = move_forward)
-    tim.forward(random.randint(10, 20))
+    #tim.forward(random.randint(10, 20))
     jim.forward(random.randint(10, 20))
     kelly.forward(random.randint(10, 20))
     miya.forward(random.randint(10, 20))
