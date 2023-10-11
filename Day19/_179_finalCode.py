290f06b7d07291da90663535afa7479133500655
SK <sunil.kafle@hotmail.com>
2023-10-11 12:18:14 -0400

    code changed

--- Day19/_179_finalCode.py
+++ Day19/_179_finalCode.py
@@ -1,2 +1,14 @@
 from turtle import Turtle, Screen
 import random
+
+screen = Screen()
+screen.setup(width= 500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt= "Which turtle will win: ")
+print(user_bet)
+colors = []
+
+tim = Turtle()
+tim.penup()
+tim.goto(x=-240, y = 0)
+
+screen.exitonclick()
