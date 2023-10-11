1917100ef25c44abb9e93b1bae16883974d4a70a
SK <sunil.kafle@hotmail.com>
2023-10-11 12:44:27 -0400

    code changed

--- Day19/_179_finalCode.py
+++ Day19/_179_finalCode.py
@@ -1,18 +1,26 @@
 from turtle import Turtle, Screen
 import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width= 500, height=400)
-# user_bet = screen.textinput(title="Make your bet", prompt= "Which turtle will win: ")
-# print(user_bet)
+user_bet = screen.textinput(title="Make your bet", prompt= "Which turtle will win: ")
+print(user_bet)
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_position = [-100, -50, 0, 50, 100, 150]
+all_turtles = []
 for i in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(colors[i])
-    tim.goto(x=-230, y = y_position[i])
-    
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[i])
+    new_turtle.goto(x=-230, y = y_position[i])
+    all_turtles.append(new_turtle)
+if user_bet:
+    is_race_on = True
+while is_race_on:
+    for turtle in all_turtles:
+        distance = random.randint(0, 10)
+        turtle.forward(distance)
 
 
 screen.exitonclick()
