7ff5bba7d2ec185c899ade4fdc16897c32dc08a6
SK <sunil.kafle@hotmail.com>
2023-10-12 12:11:08 -0400

    code changed

--- Day21/_190_ClassInheritance.py
+++ Day21/_190_ClassInheritance.py
@@ -3,3 +3,15 @@ class Animal:
         self.num_eyes= 2
     def breathe(self):
         print("Inhale, exhale.")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__() # inherit attributes and methods from the Animal class.
+
+    def swim(self):
+        print("Moving in the water.")
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
