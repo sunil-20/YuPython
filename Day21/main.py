eeb98f4547b7530362f8d10a7a7eafa8a6f76101
SK <sunil.kafle@hotmail.com>
2023-10-13 15:55:07 -0400

    code changed

--- Day21/main.py
+++ Day21/main.py
@@ -30,5 +30,7 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.calc_score()
-
+    # Detech collision with the wall
+    if snake.head.xcor()> 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() <-280:
+        game_is_on = False
 screen.exitonclick()
