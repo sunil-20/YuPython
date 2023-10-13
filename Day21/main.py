00c678b9af11395d5213728d5699f64124b3b5e2
SK <sunil.kafle@hotmail.com>
2023-10-13 13:58:06 -0400

    code changed

--- Day21/main.py
+++ Day21/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from snake import Snake
 import time
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,5 +29,6 @@ while game_is_on:
     # Detect collision with food using distance method
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.calc_score()
 
 screen.exitonclick()
