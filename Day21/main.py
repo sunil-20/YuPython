51ac0508ff04895691a1dd579f6aa7484aa0a0e7
SK <sunil.kafle@hotmail.com>
2023-10-25 11:59:59 -0400

    code changed

--- Day21/main.py
+++ Day21/main.py
@@ -37,7 +37,9 @@ while game_is_on:
         scoreboard.game_over()
     # Detect collision with tail
     for segment in snake.segments:
-        if snake.head.distance(segment)<10:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment)<10:
             game_is_on = False
             scoreboard.game_over()
 screen.exitonclick()
