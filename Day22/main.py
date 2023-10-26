64fdb698f1c7b31f1b3e142d3dee1dad865d1aa6
SK <sunil.kafle@hotmail.com>
2023-10-26 15:30:36 -0400

    code changed

--- Day22/main.py
+++ Day22/main.py
@@ -1,1 +1,11 @@
 # ping pong
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+screen.tracer(0)
+
+
+screen.exitonclick()
