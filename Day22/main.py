6f3239d8e365f92317855a794a9c4841dd013c37
SK <sunil.kafle@hotmail.com>
2023-10-26 15:39:03 -0400

    code changed

--- Day22/main.py
+++ Day22/main.py
@@ -5,11 +5,12 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong")
-screen.tracer(0)
+
 
 # creating paddle on both sides of the screen
-paddle = Turtle
+paddle = Turtle()
 paddle.shape("square")
+paddle.color("white")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 
 
