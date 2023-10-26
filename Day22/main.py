a389cc3a504e61ee5bc365e88473eebb2cc4c86b
SK <sunil.kafle@hotmail.com>
2023-10-26 15:35:46 -0400

    code changed

--- Day22/main.py
+++ Day22/main.py
@@ -1,11 +1,16 @@
 # ping pong
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
+
+# creating paddle on both sides of the screen
+paddle = Turtle
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
 
 
 screen.exitonclick()
